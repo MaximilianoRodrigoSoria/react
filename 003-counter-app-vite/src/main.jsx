35fe5c19a90cf1a6a7f295193d16ee9c3f50c317
Maximiliano R Soria <maximilianorodrigosoria@gmail.com>
2022-06-13 06:58:03 -0300

    Hola Mundo react vite
    Hola Mundo react vite

--- 003-counter-app-vite/src/main.jsx
+++ 003-counter-app-vite/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function App(){
+  return (<h1>Hola mundo!!!</h1>);
+}
+
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
   <React.StrictMode>
-    <App />
+    <App></App>
   </React.StrictMode>
 )
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
