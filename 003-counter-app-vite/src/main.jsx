326913169f7587a644dbdbc0147c69e965b827cd
Maximiliano R Soria <maximilianorodrigosoria@gmail.com>
2022-06-13 07:51:18 -0300

    Estilos globales
    Estilos globales

--- 003-counter-app-vite/src/main.jsx
+++ 003-counter-app-vite/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import HelloWordApp  from './HelloWord';
 import AppPrueba from './PruebaComponent';
 
-
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
