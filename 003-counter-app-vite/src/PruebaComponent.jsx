4477be1ae52189fafc93db4239186fd4de0cf4bc
Maximiliano R Soria <maximilianorodrigosoria@gmail.com>
2022-06-13 07:29:49 -0300

    Fragment
    Fragment

---
+++ 003-counter-app-vite/src/PruebaComponent.jsx
@@ -0,0 +1,10 @@
+
+const appPrueba = () => {
+ return (
+ <>
+    <h2>Esto es una prueba de componente</h2>
+    <p>Esto es un parrafo</p>
+ </>)
+};
+
+export default appPrueba;
--- 003-counter-app-vite/src/main.jsx
+++ 003-counter-app-vite/src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import HelloWordApp  from './HelloWord';
+import AppPrueba from './PruebaComponent';
+
+
 
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
   <React.StrictMode>
     <HelloWordApp></HelloWordApp>
+    <AppPrueba></AppPrueba>
   </React.StrictMode>
 )
 ;
